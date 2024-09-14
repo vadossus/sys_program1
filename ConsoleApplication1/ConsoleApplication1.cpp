456c4390805a07b4190ddf3ea930b864ee945d31
vadossus <157449023+vadossus@users.noreply.github.com>
2024-09-14 13:38:19 +0500

    Add files via upload

---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,47 @@
+﻿// ConsoleApplication1.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+
+int main()
+{
+    setlocale(0, "rus");
+    
+    /* int a, int b;
+    std::cout << "Введите число А:\n";
+    std::cin >> a;
+    std::cout << "Введите число B:\n";
+    std::cin >> b;
+    int result = a * b;
+    std::cout << "Результат умножения двух чисел A и B: " << a << std::endl; */
+
+    int a;
+    bool res = false;
+    std::cout << "Введите число А:\n";
+    std::cin >> a;
+    
+    if (a >= 1) {
+
+        for (int i = 2; i * i <= a; i++)
+        {
+            if (a % i == 0)
+            {
+                res = true;
+                break;
+            }
+        }
+
+        if (res)
+        {
+            std::cout << "Число не является простым" << std::endl;
+        }
+        else
+        {
+            std::cout << "Число простое" << std::endl;
+        }
+    }
+    else
+    {
+        std::cout << "Число не является простым" << std::endl; 
+    }
+}
